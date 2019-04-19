4c020e10cab07efd17e747a326370d2ea169510b
glitch-ss <547076180@qq.com>
2019-04-19 23:35:09 +0800

    1.0.0_r20190419

---
+++ app/do_sql.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+class do_sql():
+    def __init__(self):
+        self.conn=mysql.connector.connect(host='localhost',user='root',passwd="Atobefuji.900209", port=3306, db='purchase')
+        self.cursor=conn.cursor()
+    
+    def send_cmd(self,cmd,err):
+        try:
+            result = self.cursor.execute(cmd)
+            self.conn.commit()
+        except Exception, e:
+            print e
+            print err
+    
+    def add_item(self, id, name, num, price, status, description):
+        cmd = 'insert into items value({0}, "{1}" , {2}, {3}, {4}, "{5}");'.format(id,name, num, price, status, description)
+        self.send_cmd(cmd, "fail to add data")
+    
+    def set_item(self, id, property, val):
+        if isinstance(val,str):
+            val='"'+val+'"'
+        cmd = 'update items set {0} = {1} where id={3};'.format(property, val, id)
+        self.send_cmd(cmd, "fail to set data")
+        
+    def get_item_by_id(self, id):
+        cmd = 'select * from items where id={0};'.format(id)
+        self.send_cmd(cmd, "fail to get data by id")
+        
+    def get_items(self):
+        cmd = 'select * from items;'
+        self.send_cmd(cmd, "fail to get datas")
---
+++ app/views.py
@@ -0,0 +1,33 @@
+#-*- coding:utf-8
+from app import app
+from flask import json, jsonify,request
+from app import do_sql
+
+s=do_sql.do_sql()
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello World"
+
+@app.route('/getlist', methods=['POST'])
+def form_data():
+    print "#####"
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    print type(json_data)
+    return jsonify({'status': '0', 'errmsg': u'登录成功！'})
+
+@app.route('/additem',methods=['POST'])
+def add_data():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    if id in json_data:
+        id = json_data['id']
+    else:
+        return jsonify({'status': '-1', 'errmsg': u'id not set, wrong set'})
+    number = json_data['number']
+    name = json_data['name']
+    price = json_data['price']
+    status = json_data['status']
+    description =json_data['description']
+    s.add_item(id, name, number, price, status, description)
