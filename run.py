acec1d6fb1560b0e847caf78f92a934f545ebe48
glitch-ss <547076180@qq.com>
2019-04-21 12:14:24 +0800

    1.0.0.0_r20190421_1214

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 #!flask/bin/python
 from app import app
-app.run(debug = True, host='0.0.0.0', port=5000, ssl_context=('../rsa/server.crt','../rsa/server.key'))
+
+if __name__=='__main__':
+    app.run(debug = True)
