949af86bf610ed7ccd2a0b912ce5af48ef9e9c99
glitch-ss <547076180@qq.com>
2019-09-27 10:44:53 +0800

    ;

--- app/monitor/sephora.py
+++ app/monitor/sephora.py
@@ -106,7 +106,7 @@ class Sephora():
             print k
 
     def get_item_url_by_id(self, id):
-        search_url = "https://www.sephora.com/search?keyword=" + str(id)rrrr
+        search_url = "https://www.sephora.com/search?keyword=" + str(id)
         response = self.s.get(
             search_url, headers=self.normal_headers, allow_redirects=False)
         return response.headers['location']
